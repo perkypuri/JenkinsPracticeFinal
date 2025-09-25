42999a4dc1981d8b11aabca528732c8e8721a4c1
RPC <ramisettipurnachandrikcd..>
2025-09-25 09:45:01 +0530

    commit

--- demopractice/vite.config.js
+++ demopractice/vite.config.js
@@ -1,17 +1,7 @@
-/*import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/apiuser/',  
 })
-*/
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/apiuser/',  // your frontend context in Tomcat
-});
